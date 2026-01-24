33493aa8e70660c2f1064954e0cae9f5dff1b023
Tanaka Makuvaza <tmaku12430@gmail.com>
2026-01-24 03:34:43 -0500

    fix: Resolve Card class naming conflict between Flutter and Stripe

--- mobile/lib/features/payment/payment_screen.dart
+++ mobile/lib/features/payment/payment_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:flutter_stripe/flutter_stripe.dart' hide Card;
 import '../../core/services/api_service.dart';
 
 class PaymentScreen extends StatefulWidget {
