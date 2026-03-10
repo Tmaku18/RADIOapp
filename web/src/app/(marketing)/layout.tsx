da6bfb57506384775448f6d5fd78a867ca89ca1d
Tanaka Makuvaza <tmaku12430@gmail.com>
2026-03-10 06:39:26 -0400

    Header CTA: Get Started -> Sign Up/Login

--- web/src/app/(marketing)/layout.tsx
+++ web/src/app/(marketing)/layout.tsx
@@ -69,7 +69,7 @@ export default function MarketingLayout({
             {/* Auth + ProNetworx: one blue CTA, ProNetworx far right */}
             <div className="flex items-center space-x-2">
               <Button asChild className="bg-networx text-black hover:opacity-90">
-                <Link href="/signup">Get Started</Link>
+                <Link href="/signup">Sign Up/Login</Link>
               </Button>
               <Button asChild className="bg-networx text-black hover:opacity-90">
                 <Link href="/pro-networx">ProNetworx</Link>
